3b0f859a1222dfed80baa5eab3cb90917dd3974e
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-08 16:03:52 -0300

    feat: :sparkles: Added lesson 4.

--- courses/course 1/lesson 4/zombiehelper.sol
+++ courses/course 1/lesson 4/zombiehelper.sol
@@ -3,24 +3,38 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+    uint levelUpFee = 0.001 ether;
+
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
     }
 
+    function withdraw() external onlyOwner {
+        address _owner = owner();
+        _owner.transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
+
     function changeName(
         uint _zombieId,
         string calldata _newName
-    ) external aboveLevel(2, _zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    ) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
         zombies[_zombieId].name = _newName;
     }
 
     function changeDna(
         uint _zombieId,
         uint _newDna
-    ) external aboveLevel(20, _zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    ) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
         zombies[_zombieId].dna = _newDna;
     }
 
@@ -29,14 +43,12 @@ contract ZombieHelper is ZombieFeeding {
     ) external view returns (uint[] memory) {
         uint[] memory result = new uint[](ownerZombieCount[_owner]);
         uint counter = 0;
-
         for (uint i = 0; i < zombies.length; i++) {
             if (zombieToOwner[i] == _owner) {
                 result[counter] = i;
                 counter++;
             }
         }
-
         return result;
     }
 }
